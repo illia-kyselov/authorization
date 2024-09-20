a2f5c560e1afa66df61f076cb5424c46a43b26ae
_PaLa4_ <illya.kis99@gmail.com>
2024-09-20 15:59:52 +0300

    fix routes and added new comp

---
+++ src/pages/NavLinks/NavLinks.tsx
@@ -0,0 +1,27 @@
+import { Link, useLocation } from "@tanstack/react-router";
+
+const MenuComponent: React.FC = () => {
+    const location = useLocation();
+
+    const isHomeActive = location.pathname === "/authorization";
+    const isLoginActive = location.pathname.includes("/authorization/login");
+
+    return (
+        <div className="p-3 flex gap-5 ml-4">
+            <Link
+                to="/authorization/"
+                className={`font-semibold text-lg ${isHomeActive ? "!font-bold text-green-800" : ""}`}
+            >
+                Home
+            </Link>
+            <Link
+                to="/authorization/login"
+                className={`font-semibold text-lg ${isLoginActive ? "!font-bold text-green-800" : ""}`}
+            >
+                Login
+            </Link>
+        </div>
+    );
+};
+
+export default MenuComponent;
